3073e909e17e6dbc8064be5f738cbc68489921d6
Julian Hicks <jkballin@gmail.com>
2019-06-24 20:17:19 -0700

    updates

--- model/estate.js
+++ model/estate.js
@@ -3,6 +3,44 @@ const Schema = mongoose.Schema;
 
 const estateSchema = new Schema({
 
+    estatetitle: {
+        type: String,
+        required: true
+    },
+    for: {
+        type: String,
+        required: true
+    },
+    bedrooms: {
+        type: String,
+        required: true
+    },
+
+    bathrooms: {
+        type: String,
+        required: true
+    },
+
+    garage: {
+        type: Boolean,
+        required: true
+    },
+
+    contact: {
+        type: String,
+        required: true
+    },
+
+    city: {
+        type: String,
+        required: true
+    },
+
+    date: {
+        type: Date,
+        required: Date.now
+    },
+
     image: {
         type: String,
         required: true
