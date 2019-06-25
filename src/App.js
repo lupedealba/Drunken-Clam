8169bff0b1ef8eeb963d27721780d42480ba43be
Brandon White <46477427+iambrandonwhite@users.noreply.github.com>
2019-06-24 20:42:40 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './components/Hero/index.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Beer <code> </code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <h1>Real Estate Finder</h1>
+        <p>Bringing property right to your fingertips</p> 
+        
       </header>
     </div>
   );
