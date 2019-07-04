156962ab1e3904f6da2f135d5ed80e7d86a4b069
Brandon White <brandonwhite4118@gmail.com>
2019-07-03 21:22:39 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 // import './App.css';
-import './components/Hero/index.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
-      <h1>Real Estate Finder</h1>
-        <p>Bringing property right to your fingertips</p> 
+      <h1>Maverick Move</h1>
+        <p>Bringing property right to your fingertips</p>
+        <textarea>City, State</textarea>
+        <button>Search</button>
         
       </header>
+      <body>
+        <p>Tell us where you would like to move</p>
+      </body>
     </div>
   );
 }
