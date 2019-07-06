24797488052b3b288f4225fef6c132fd312bb97b
b-widg <b-widg@users.noreply.github.com>
2019-07-06 15:58:54 -0700

    m

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './Components/NavBar';
+import Cards from './Components/Card';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar />
+      <Cards />
+      {/* <header className="App-header">
 
       <h1>Maverick Move</h1>
         <p>Bringing property right to your fingertips</p>
@@ -14,7 +18,7 @@ function App() {
       </header>
       <body>
         <p>Tell us where you would like to move</p>
-      </body>
+      </body> */}
     </div>
   );
 }
