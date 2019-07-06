1b581fff0addf66652fce8a8661159816d5bbe88
loopy muchacha <lupegdealba@gmail.com>
2019-07-06 14:04:37 -0700

    added icon

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -13,7 +13,7 @@ import "./Navbar.css";
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
   <img src={logo} width="100" height="100" alt="Logo" />
-  <a className="navbar-brand" href="#">Maverick Move</a>
+  <a className="navbar-brand app-title" href="#">Maverick Move</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
