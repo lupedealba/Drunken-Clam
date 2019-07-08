086c9a3cd1bddd8bddcf37de292da8151a555ad1
loopy muchacha <lupegdealba@gmail.com>
2019-07-07 23:31:09 -0700

    commit before git pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar/Navbar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Cards from './components/Card';
 import Signin from './components/Signin';
 import Register from './components/Register';
